32ded38cb18d9b947b7239cafd4c8bcbea4a7725
yak <yakxxx@gmail.com>
2012-12-31 10:34:11 +0100

    test refact.

--- crawler/tests.py
+++ crawler/tests.py
@@ -16,79 +16,73 @@ class ApiTest(unittest.TestCase):
         super(ApiTest, self).__init__(*args, **kwargs)
         a = Api()
         self.promoted = a.get_promoted()
+        
+    def setUp(self):
+        self.a = Api()
     
     def test_make_api_params_str(self):
-        a = Api()
-        api_str = a._make_api_params_str({'a':1, 'b': 'asd'})
+        api_str = self.a._make_api_params_str({'a':1, 'b': 'asd'})
         self.assertTrue(api_str == u'a,1,b,asd' or api_str == u'b,asd,a,1')
         
-        api_str2 = a._make_api_params_str({})
+        api_str2 = self.a._make_api_params_str({})
         self.assertEqual(api_str2, '')
     
     def test_make_method_params(self):
-        a = Api()
-        ret = a._make_method_params_str(['asd','xxx',123])
+        ret = self.a._make_method_params_str(['asd','xxx',123])
         self.assertEqual(ret, u'asd/xxx/123')
         
     def test_bad_get(self):
-        a = Api()
-        self.assertRaises(ApiError, a._get, 'bla', ['a',1,2], {'ax':123,'by':'qqq'})
+        self.assertRaises(ApiError, self.a._get, 'bla', ['a',1,2], {'ax':123,'by':'qqq'})
         
     def test_is_resp_ok(self):
         resp = Mock()
-        a = Api()
         resp.status_code = 200
         resp.json = {'error': {'code': 1, 'message': 'xaxaxa'}}
-        self.assertFalse(a._is_resp_ok(resp))
+        self.assertFalse(self.a._is_resp_ok(resp))
 
         #False
         resp.reset_mock()
         resp.status_code = 404
         resp.json = ''
-        self.assertFalse(a._is_resp_ok(resp))
+        self.assertFalse(self.a._is_resp_ok(resp))
         
         resp.reset_mock()
         resp.status_code = 404
         resp.json = [1,2,3,4]
-        self.assertFalse(a._is_resp_ok(resp))
+        self.assertFalse(self.a._is_resp_ok(resp))
     
         #True
         resp.reset_mock()
         resp.status_code = 200
         resp.json = [1,2,3,4]
-        self.assertTrue(a._is_resp_ok(resp))
+        self.assertTrue(self.a._is_resp_ok(resp))
         
     def test_get_promoted(self):
-        a = Api()
-        promoted = a.get_promoted()
+        promoted = self.a.get_promoted()
         self.assertGreater(len(promoted[0]['title']), 0)
         
     def test_get_comments(self):
-        a = Api()
-        link_id = a.parse_link_id_from_url(self.promoted[0]['url'])
+        link_id = self.a.parse_link_id_from_url(self.promoted[0]['url'])
         self.assertTrue(re.match(r'[0-9]*', link_id))
 
-        comments = a.get_comments(link_id)
+        comments = self.a.get_comments(link_id)
         self.assertGreater(len(comments), 0)
         
     def test_load_comments_into_model(self):
-        a = Api()
-        link_id = a.parse_link_id_from_url(self.promoted[0]['url'])
-        comments = a.get_comments(link_id)
+        link_id = self.a.parse_link_id_from_url(self.promoted[0]['url'])
+        comments = self.a.get_comments(link_id)
         model_comment = Comment(**comments[0])
         self.assertEqual(model_comment.comment_id, comments[0]['id'])
 
     def test_load_articles_into_model(self):
-        a = Api()
         art = Article(**self.promoted[0])
         self.assertEqual(art.article_id, self.promoted[0]['id'])
         
     def test_parse_link_id(self):
         url = u'http://www.wykop.pl/link/1363053/maszyny-ktorych-jeszcze-nie-znamy/'
-        a = Api()
-        link_id = a.parse_link_id_from_url(url)
+        link_id = self.a.parse_link_id_from_url(url)
         self.assertEqual(link_id, '1363053')
-        self.assertRaises(WrongData, a.parse_link_id_from_url, 'xxaxaxaxa' )
+        self.assertRaises(WrongData, self.a.parse_link_id_from_url, 'xxaxaxaxa' )
         
     
 class WykopTest(unittest.TestCase):    
